69eb278e09e6e0d34d09e32d8d82c987da571ec5
Ugonz86 <ugonzalez86@gmail.com>
2020-01-10 12:59:47 -0800

    Add Vendor class.

--- Tracker/Models/Vendor.cs
+++ Tracker/Models/Vendor.cs
@@ -2,5 +2,21 @@ using System.Collections.Generic;
 
 namespace Tracker.Models
 {
-   
+   public class Vendor
+   {
+       private static List<Vendor> _instances = new List<Vendor> { };
+       public string Name { get; set; }
+       public int Id { get; }
+       public List<Order> Orders { get; set; }
+
+       public Vendor(string vendorName)
+       {
+           Name = vendorName;
+           _instances.Add(this);
+           Id = _instances.Count;
+           Orders = new List<Order> { };
+       }
+
+      
+   }
 }
