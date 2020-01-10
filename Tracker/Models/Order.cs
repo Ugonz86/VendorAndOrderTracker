9c67edd14e138a872d9c46bbe27df8ee62963235
Ugonz86 <ugonzalez86@gmail.com>
2020-01-10 13:15:21 -0800

    Add Order class.

--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -2,5 +2,23 @@ using System.Collections.Generic;
 
 namespace Tracker.Models
 {
-   
+   public class Order
+   {
+       public string Description { get; set; }
+       public string Quantity { get; set; }
+       public string Price { get; set; }
+       public string Date { get; set; }
+       public int Id { get; set; }
+       private static List<Order> _instances = new List<Order> { };
+
+       public Order(string description, string quantity, string price, string date )
+        {
+            Description = description;
+            Quantity = quantity;
+            Price = price;
+            Date = date;
+            _instances.Add(this);
+            Id = _instances.Count;
+        }
+   }
 }
--- Tracker/Models/Vendor.cs
+++ Tracker/Models/Vendor.cs
@@ -5,13 +5,13 @@ namespace Tracker.Models
    public class Vendor
    {
        private static List<Vendor> _instances = new List<Vendor> { };
-       public string Name { get; set; }
+       public string Description { get; set; }
        public int Id { get; }
        public List<Order> Orders { get; set; }
 
        public Vendor(string vendorName)
        {
-           Name = vendorName;
+           Description = vendorName;
            _instances.Add(this);
            Id = _instances.Count;
            Orders = new List<Order> { };
@@ -30,6 +30,11 @@ namespace Tracker.Models
         public static List<Vendor> GetAll()
         {
             return _instances;
+        }
+
+        public static Vendor Find(int searchId)
+        {
+            return _instances[searchId - 1];
         }
    }
 }
