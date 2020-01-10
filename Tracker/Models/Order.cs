4b734ae81fbaab8de66fbb6303d605aedf770404
Ugonz86 <ugonzalez86@gmail.com>
2020-01-10 13:28:28 -0800

    Add Order class.

--- Tracker/Models/Order.cs
+++ Tracker/Models/Order.cs
@@ -20,5 +20,20 @@ namespace Tracker.Models
             _instances.Add(this);
             Id = _instances.Count;
         }
+
+        public static List<Order> GetAll()
+        {
+            return _instances;
+        }
+
+        public static Order Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
+
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
    }
 }
